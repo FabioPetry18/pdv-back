cc833edacedd32ffa9a8efcb409f5f7897af295d
FabioPetry18 <fabiopetry1@hotmail.com>
2023-12-27 22:45:18 -0300

    Ajuste de configuração de CORS.

--- src/main/java/com/petry/pdv/security/SecurityConfig.java
+++ src/main/java/com/petry/pdv/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
        return http
-    	   .csrf((csrf -> csrf.disable()))    
+    	   .csrf((csrf -> csrf.disable()))
+    	   .cors(cors -> cors.configurationSource(corsConfigurationSource())) 
            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
            .authorizeHttpRequests(authorize -> authorize
         		   .requestMatchers(HttpMethod.POST, "/login").permitAll()
@@ -57,22 +58,25 @@ public class SecurityConfig  {
     public PasswordEncoder passwordEncoder() {
     	return new BCryptPasswordEncoder();
     }
+
     
     @Bean
-    public CorsFilter corsFilter() {
-    	CorsConfiguration corsConfiguration = new CorsConfiguration();
-    	corsConfiguration.setAllowCredentials(true);
-    	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-    	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-    			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-    			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-    	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-    			"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-    	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-    	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-    	return new CorsFilter(urlBasedCorsConfigurationSource);
+    public CorsConfigurationSource corsConfigurationSource() {
+       CorsConfiguration corsConfiguration = new CorsConfiguration();
+       corsConfiguration.setAllowCredentials(true);
+       corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+       corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+           "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+           "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+       corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+           "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+       corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**", corsConfiguration);
+       return source;
     }
+    
+
 
 
 }
